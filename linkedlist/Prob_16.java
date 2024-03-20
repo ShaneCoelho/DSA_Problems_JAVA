edc79bc4bd926fee5225cdb0e41b3ce257054519
ShaneCoelho <shanecoelho1@gmail.com>
2024-03-20 23:04:50 +0530

    sloved problem 16 and 17

---
+++ linkedlist/Prob_16.java
@@ -0,0 +1,155 @@
+/* Clone a linked list with next and random pointer. */
+
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+    Node random;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.random=null;
+    }
+}
+
+class LinkedList {
+    Node temp1, temp2, original_head = null, clone_head = null, original_node=null, clone_node=null;
+
+    public void add_beg(int data) {
+        Node newNode = new Node(data);
+        if (original_head == null) {
+            original_head = newNode;
+        } else {
+            newNode.next = original_head;
+            original_head = newNode;
+        }
+    }
+
+    public void display(Node head) {
+        temp1 = head;
+        while (temp1 != null) {
+            System.out.println(temp1.data);
+            temp1 = temp1.next;
+        }
+    }
+
+    public void add_random(){
+        Scanner ip = new Scanner(System.in);
+        temp1=original_head;
+        while(temp1!=null){
+            System.out.println("Enter the random for "+temp1.data);
+            int random_pointer = ip.nextInt();
+            temp2=original_head;
+            while(temp2.data!=random_pointer){
+                temp2=temp2.next;
+            }
+            temp1.random=temp2;
+            temp1=temp1.next;
+        }
+    }
+
+    public Node clone() {
+        temp1 = original_head;
+        while (temp1 != null) {
+            if (clone_head == null) {
+                Node newNode = new Node(temp1.data);
+                clone_head = temp2 = newNode;
+            } else {
+                Node newNode = new Node(temp1.data);
+                temp2.next = newNode;
+                temp2 = temp2.next;
+            }
+            temp1 = temp1.next;
+        }
+    
+        return clone_head;
+    }
+
+    public void check_random(){
+        temp1=clone_head;
+
+        for(int i=0;i<5;i++){
+            System.out.println(temp1.data);
+            temp1=temp1.random;
+        }
+    }
+
+    public void join_nodes(){
+        original_node=original_head;
+        clone_node=clone_head;
+
+        while(clone_node!=null){
+            temp1=original_node.next;
+            original_node.next=clone_node;
+            original_node=temp1;
+            temp2=clone_node.next;
+            clone_node.next=original_node;
+            clone_node=temp2;
+
+        }
+    }
+
+    public void clone_random(){
+        original_node=original_head;
+        clone_node=clone_head;
+
+        while(original_node!=null){
+            if(original_node.next!=null){
+                if(original_node.random!=null){
+                    original_node.next.random=original_node.random.next;
+                }
+                else{
+                    original_node.next=original_node.random;
+                }
+            }
+            
+            original_node=original_node.next.next;
+        }
+    }
+
+    public void break_chain(){
+        original_node=original_head;
+        clone_node=clone_head;
+
+        while(clone_node.next!=null && original_node.next!=null){
+            original_node.next=clone_node.next;
+            original_node=original_node.next;
+            clone_node.next=original_node.next;
+            clone_node=clone_node.next;
+        }
+    }
+    
+}
+
+public class Prob_16 {
+
+    public static void main(String[] args) {
+        LinkedList l1 = new LinkedList();
+
+        l1.add_beg(2);
+        l1.add_beg(3);
+        l1.add_beg(5);
+        l1.add_beg(8);
+        Node clone_head = l1.clone();
+        System.out.println("Original:");
+        l1.display(l1.original_head);
+        System.out.println("Clone:");
+        l1.display(clone_head);
+        l1.add_random();
+        // l1.check_random();
+        l1.join_nodes();
+        System.out.println("Joint Nodes");
+        l1.display(l1.original_head);
+        l1.clone_random();
+        l1.break_chain();
+        System.out.println("Original node:");
+        l1.display(l1.original_head);
+        System.out.println("Clone node:");
+        l1.display(l1.clone_head);
+        System.out.println("Random:");
+        l1.check_random();
+
+    }
+}
---
+++ linkedlist/Prob_17.java
@@ -0,0 +1,106 @@
+/*Detect a palindrome linked list. */
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Palindrome {
+
+    Node temp, slow, fast, mid, reverse, curr, prev, after, temp1, temp2, head = null;
+
+    public void add_beg(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void display() {
+        temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public void find_mid() {
+        slow=head;
+        fast=head.next;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        mid=slow;
+
+    }
+
+    public void reverse_after_mid(){
+        curr=mid.next;
+        prev=null;
+        after=null;
+
+        while(curr!=null){
+            after=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=after;
+        }
+        reverse=prev;
+        mid.next=reverse;
+
+    }
+
+    public boolean check_palindrome(){
+        temp1=head;
+        temp2=mid.next;
+
+        if(temp1.next==null){
+            return true;
+        }
+
+        while(temp2!=null){
+            if(temp1.data==temp2.data){
+                temp1=temp1.next;
+                temp2=temp2.next;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class Prob_17 {
+    public static void main(String[] args) {
+        Palindrome p1 = new Palindrome();
+
+        p1.add_beg(1);
+        p1.add_beg(2);
+        p1.add_beg(2);
+        p1.add_beg(1);
+        p1.display();
+        p1.find_mid();
+        p1.reverse_after_mid();
+        System.out.println("After Reverse:");
+        p1.display();
+        boolean result=p1.check_palindrome();
+        if(result==true){
+            System.out.println("The Linked List is a palindrome");
+        }else{
+            System.out.println("The Linked List is not a palindrome");
+        }
+
+    }
+
+}
